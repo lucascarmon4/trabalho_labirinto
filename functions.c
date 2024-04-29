c349d6fe68e8269b6925c4540b5d38059bbe5ab4
Lucas Carmona Neto <lucascarmonaneto510@gmail.com>
2024-04-29 09:48:01 -0300

    docs

--- functions.c
+++ functions.c
@@ -4,20 +4,36 @@
 #include <unistd.h>
 #include <windows.h>
 
+
+/*
+* Função que inicializa a pilha.
+*/
 void initStack(Stack *s) {
     s->top = -1;
 }
 
+/*
+* Função que verifica se a pilha está vazia.
+* Retorna 1 se a pilha estiver vazia, 0 caso contrário.
+*/
 int isStackEmpty(Stack *s) {
     return s->top == -1;
 }
 
+/*
+* Função que insere um elemento no topo da pilha.
+* Se a pilha estiver cheia, não faz nada.
+*/
 void push(Stack *s, Position pos) {
     if (s->top < MAX_SIZE * MAX_SIZE - 1) {
         s->positions[++s->top] = pos;
     }
-}
+} 
 
+/*
+* Função que remove e retorna o elemento do topo da pilha.
+* Se a pilha estiver vazia, retorna uma posição inválida (-1, -1).
+*/
 Position pop(Stack *s) {
     if (!isStackEmpty(s)) {
         return s->positions[s->top--];
@@ -25,6 +41,12 @@ Position pop(Stack *s) {
     return (Position){-1, -1};
 }
 
+/*
+* Função que carrega o labirinto a partir de um arquivo labirinto.txt.
+* O arquivo deve ter o seguinte formato:
+*   <número de linhas> <número de colunas>
+*   <labirinto> (0 para caminho livre, X para parede, S para queijo)
+*/
 void loadMaze(Maze *maze, const char *filename) {
     FILE *file = fopen(filename, "r");
     if (!file) {
@@ -36,19 +58,20 @@ void loadMaze(Maze *maze, const char *filename) {
     fgetc(file);
 
     for (int i = 0; i < maze->rows; i++) {
-        fgets(maze->maze[i], maze->cols + 2, file); // +2 para o newline e o null-terminator
+        fgets(maze->maze[i], maze->cols + 2, file); // +2 => inclui o '\n' e o '\0'
         maze->maze[i][maze->cols] = '\0';
     }
     fclose(file);
 }
 
-
+/*
+* Função que explora o labirinto a partir de uma posição inicial.
+* Retorna 1 se encontrar o queijo, 0 caso contrário.    
+*/
 int exploreMaze(Maze *maze, Position start, int visited[MAX_SIZE][MAX_SIZE]) {
     Stack path;
     initStack(&path);
     push(&path, start);
-
-    visited[start.x][start.y] = 1;
 
     Position lastPosition = {-1, -1}; 
 
@@ -61,19 +84,16 @@ int exploreMaze(Maze *maze, Position start, int visited[MAX_SIZE][MAX_SIZE]) {
             }
         }
 
-        // Checa se alcançou a saída antes de qualquer ação
         if (maze->maze[current.x][current.y] == 'S') {
-            maze->maze[current.x][current.y] = 'R';  // Marca a saída como queijo 'Q'
+            maze->maze[current.x][current.y] = 'R';  
             displayMaze(maze);
             return 1;
         }
 
-        // Marca a posição atual com 'R' e exibe o labirinto
         maze->maze[current.x][current.y] = 'R';
         displayMaze(maze);
         Sleep(200);
 
-        // Atualiza a última posição
         lastPosition = current; 
 
         Position adjPositions[4] = {
@@ -94,23 +114,29 @@ int exploreMaze(Maze *maze, Position start, int visited[MAX_SIZE][MAX_SIZE]) {
             }
         }
 
-        // Se nenhum movimento válido foi encontrado e a posição atual é uma parede ou já marcada, mantenha 'R'
         if (!validMoveFound && maze->maze[current.x][current.y] == 'R') {
-            maze->maze[current.x][current.y] = 'R';  // Mantém 'R' na última posição se não houver movimento válido
+            maze->maze[current.x][current.y] = 'R';  
         }
     }
 
-    return 0;  // Saída não encontrada
+    return 0; 
 }
 
-
-
-
+/*
+* Função que verifica se um movimento é válido.
+* Retorna 1 se o movimento é válido, 0 caso contrário.
+*/
 int isValidMove(Maze *maze, int x, int y) {
-    if (x < 0 || x >= maze->rows || y < 0 || y >= maze->cols) return 0; // Fora dos limites do labirinto
+    if (x < 0 || x >= maze->rows || y < 0 || y >= maze->cols) return 0;
     char cell = maze->maze[x][y];
     return cell == '0' || cell == 'S';  // Movimento válido se for '0' ou 'S'
 }
+
+
+/*
+* Função que valida o labirinto.
+* Retorna 1 se o labirinto contém apenas '0', 'X' e 'S' e se há pelo menos uma saída, 0 caso contrário.
+*/
 int validateMaze(Maze *maze) {
     int foundExit = 0;
     for (int i = 0; i < maze->rows; i++) {
@@ -126,6 +152,11 @@ int validateMaze(Maze *maze) {
     return foundExit;
 }
 
+
+/*
+* Função que encontra a saída do labirinto a partir de uma posição inicial.
+* Retorna 1 se encontrar o queijo, 0 caso contrário.
+*/
 int findExit(Maze *maze, Position start) {
     if (maze->maze[start.x][start.y] == 'X') {
         printf("A posicao inicial esta em uma parede.\n");
@@ -138,16 +169,17 @@ int findExit(Maze *maze, Position start) {
         return 1;
     }
 
-    // Inicializa a matriz visited
     int visited[MAX_SIZE][MAX_SIZE] = {{0}};
-    // Marca a posição inicial como visitada
-    visited[start.x][start.y] = 1;
+    
+    visited[start.x][start.y] = 1; 
 
-    // A nova função exploreMaze não precisa de 'visited' como parâmetro agora
     return exploreMaze(maze, start, visited);
 }
 
-
+/*
+* Função que exibe o labirinto.
+* Não retorna nada.
+*/
 void displayMaze(Maze *maze) {
     system("cls || clear");
 
